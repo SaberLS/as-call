33a8192ca0d6e5e3b64e84a377d0eaf943f44168
SaberLS <patryk.platek19@gmail.com>
2025-10-26 00:22:13 +0200

    refactor(handlers): remove response building logic from Handlers for better separation of concerns
    - Make Handlers a normall class since abstract methods get removed
    - Add TStart generic type which serves as type of onStart handler parameters
    - Removed generic types: `TResponseBuilder`, `TFailureBuilder`
    - Removed abstract methods: `buildSuccessResponse`, `buildFailureResponse`
    - Updated `handleSuccess` and `handleFailure` to accept `response: TSuccess` / `TFailure` directly and return void

--- src/Handlers.ts
+++ src/Handlers.ts
@@ -1,44 +1,34 @@
 import { ResponseFailure, type ResponseSuccess } from './Response/Response'
 
-abstract class Handlers<
+class Handlers<
   TPayload,
   TError extends Error,
-  TSuccessBuilder extends (...args: never[]) => TSuccess,
-  TFailureBuilder extends (...args: never[]) => TFailure,
+  TStart,
   TSuccess extends ResponseSuccess<TPayload>,
   TFailure extends ResponseFailure<TError>,
 > {
-  protected onStart: OnStart | undefined
+  protected onStart: OnStart<TStart> | undefined
   protected onSuccess: OnSuccess<TSuccess> | undefined
   protected onFailure: OnFailure<TFailure> | undefined
   protected onFinal: OnFinal<TSuccess | TFailure> | undefined
 
-  constructor(handlers?: Partial<Options<TSuccess, TFailure>>) {
+  constructor(handlers?: Partial<Options<TStart, TSuccess, TFailure>>) {
     this.onStart = handlers?.onStart
     this.onSuccess = handlers?.onSuccess
     this.onFailure = handlers?.onFailure
     this.onFinal = handlers?.onFinal
   }
 
-  protected abstract buildSuccessResult: TSuccessBuilder
-  protected abstract buildFailureResult: TFailureBuilder
-
-  handleStart = () => {
-    this.onStart?.()
+  handleStart = (start: TStart) => {
+    this.onStart?.(start)
   }
 
-  handleSuccess = (...args: Parameters<TSuccessBuilder>) => {
-    const response = this.buildSuccessResult(...args)
+  handleSuccess = (response: TSuccess) => {
     this.onSuccess?.(response)
-
-    return response
   }
 
-  handleFailure = (...args: Parameters<TFailureBuilder>) => {
-    const response = this.buildFailureResult(...args)
+  handleFailure = (response: TFailure) => {
     this.onFailure?.(response)
-
-    return response
   }
 
   handleFinal = (response: TSuccess | TFailure) => {
@@ -46,8 +36,8 @@ abstract class Handlers<
   }
 }
 
-interface Options<TSuccess, TFailure> {
-  onStart: OnStart
+interface Options<TStart, TSuccess, TFailure> {
+  onStart: OnStart<TStart>
   onSuccess: OnSuccess<TSuccess>
   onFailure: OnFailure<TFailure>
   onFinal: OnFinal<TSuccess | TFailure>
@@ -55,7 +45,7 @@ interface Options<TSuccess, TFailure> {
 
 type Handler<TResponse> = (response: TResponse) => void
 
-type OnStart = () => void
+type OnStart<TStart> = (start: TStart) => void
 type OnSuccess<TSuccess> = Handler<TSuccess>
 type OnFailure<TFailure> = Handler<TFailure>
 type OnFinal<TResponse> = Handler<TResponse>
