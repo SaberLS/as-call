6c1699bbca39001a089c05241d01d91cebb03751
SaberLS <patryk.platek19@gmail.com>
2025-10-26 12:00:19 +0100

    refactor(response): unify Response types by removing ResponseSuccess and ResponseFailure classes
    Simplify type system to use a single generic Response<TPayload, TError, TSuccess>
    
    Remove ResponseSuccess and ResponseFailure aliases
    
    Enable easier extension and composition of Response in custom implementations
    
    Reduce boilerplate and improve flexibility for future Response-based abstractions
    
    BREAKING CHANGE: BREAKING CHANGE:
    - Removed ResponseSuccess and ResponseFailure classes in favor of a single generic Response<TPayload, TError, TSuccess>.
    - Existing code using ResponseSuccess or ResponseFailure must be migrated.
    
    Next:
    
    - Replace previous TGeneric extends ResponseSuccess constraints with TGeneric extends Response<TPayload, undefined, true>
    - Replace previous TGeneric extends ResponseFailure constraints with TGeneric extends Response<unknown, TError, false>

--- src/Response/Response.ts
+++ src/Response/Response.ts
@@ -1,9 +1,9 @@
-abstract class Response<TPayload, TError extends Error> {
+class Response<TPayload, TError, TSuccess extends boolean = boolean> {
   protected payload: TPayload | undefined
   protected error: TError | undefined
-  protected success: boolean
+  protected success: TSuccess
 
-  constructor(success = false, payload?: TPayload, error?: TError) {
+  constructor(success: TSuccess, payload?: TPayload, error?: TError) {
     this.payload = payload
     this.error = error
     this.success = success
@@ -22,15 +22,4 @@ abstract class Response<TPayload, TError extends Error> {
   }
 }
 
-class ResponseSuccess<TPayload> extends Response<TPayload, never> {
-  constructor(payload: TPayload) {
-    super(true, payload)
-  }
-}
-
-class ResponseFailure<TError extends Error> extends Response<unknown, TError> {
-  constructor(payload: unknown, error: TError) {
-    super(false, payload, error)
-  }
-}
-export { Response, ResponseFailure, ResponseSuccess }
+export { Response }
